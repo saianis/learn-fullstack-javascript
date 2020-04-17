3f57b763aa153832c9e08312bf94c961a3ae73aa
saianis <anish@Anishs-MacBook-Pro.local>
2020-04-18 04:06:44 +0530

    incorporated react

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const Header = ({ message }) => {
+  return (
+    <h2 className="text-center">
+      {message}
+    </h2>
+  );
+};
+
+Header.propTypes = {
+  message: PropTypes.string
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>
+        ...
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
